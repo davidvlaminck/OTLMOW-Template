9a779e321aee8ec5412109fa167efdb031762bf1
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-17 21:08:02 +0100

    initial set up

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from pathlib import Path
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from otlmow_template.SubsetTool import SubsetTool
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    SubsetTool()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print (Path('/home/davidlinux/PycharmProjects/OTLMOW-ModelBuilder/UnitTests/../../OTLMOW-Template/UnitTests/TestClasses').resolve())
