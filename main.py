5794fe4163a10fb663a3650f904b7db5546351d3
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-11-06 23:40:24 +0100

    example script

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from otlmow_template.SubsetTemplateCreator import SubsetTemplateCreator
 
 
 if __name__ == '__main__':
-    SubsetTemplateCreator()
-
-    print (Path('/home/davidlinux/PycharmProjects/OTLMOW-ModelBuilder/UnitTests/../../OTLMOW-Template/UnitTests/TestClasses').resolve())
+    stc = SubsetTemplateCreator()
+    p = Path('/home/davidlinux/Downloads/Subset VVOP-installatie.db')
+    stc.generate_template_from_subset(path_to_subset=p, path_to_template_file_and_extension=Path('VVOP.xlsx'))
