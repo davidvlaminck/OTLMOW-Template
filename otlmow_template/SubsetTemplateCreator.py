7d69081a80d4c2158bccf1ce7a0e42530744acca
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-11-06 23:40:58 +0100

    refactored to be more simple

--- otlmow_template/SubsetTemplateCreator.py
+++ otlmow_template/SubsetTemplateCreator.py
@@ -5,45 +5,19 @@ from os.path import abspath
 from pathlib import Path
 from typing import List
 
-from otlmow_converter.AssetFactory import AssetFactory
-from otlmow_converter.FileExporter import FileExporter
 from otlmow_converter.OtlmowConverter import OtlmowConverter
-from otlmow_modelbuilder.OSLOInMemoryCreator import OSLOInMemoryCreator
-from otlmow_modelbuilder.SQLDataClasses.OSLOCollector import OSLOCollector
-from otlmow_modelbuilder.SQLDbReader import SQLDbReader
+from otlmow_model.Helpers.AssetCreator import dynamic_create_instance_from_uri
+from otlmow_modelbuilder.OSLOCollector import OSLOCollector
 
 
 class SubsetTemplateCreator:
-    def __init__(self, settings_path: Path = None):
-        if settings_path is None:
-            settings_path = self._try_getting_settings_of_converter()
-        if settings_path is None:
-            raise FileNotFoundError(
-                "Instantiating this class requires a settings file. Please give a valid path to a settings file")
-        self.settings: dict = {}
-        self._load_settings(settings_path)
-
-    def _load_settings(self, settings_path):
-        if settings_path == '':
-            current_file_path = Path(__file__)
-            directory = current_file_path.parents[0]
-            settings_path = abspath(f'{directory}\\settings_sample.json')
-
-        if not os.path.isfile(settings_path):
-            raise FileNotFoundError(settings_path + " is not a valid path. File does not exist.")
-
-        try:
-            with open(settings_path) as settings_file:
-                self.settings = json.load(settings_file)
-        except OSError:
-            raise ImportError(f'Could not open the settings file at {settings_file}')
+    def __init__(self):
+        pass
 
     @staticmethod
     def _load_collector_from_subset_path(path_to_subset: Path) -> OSLOCollector:
-        sql_reader = SQLDbReader(path_to_subset)
-        oslo_creator = OSLOInMemoryCreator(sql_reader)
-        collector = OSLOCollector(oslo_creator)
-        collector.collect(include_abstract=True)
+        collector = OSLOCollector(path_to_subset)
+        collector.collect_all(include_abstract=True)
         return collector
 
     def generate_template_from_subset(self, path_to_subset: Path, path_to_template_file_and_extension: Path,
@@ -55,7 +29,7 @@ class SubsetTemplateCreator:
             class_directory = None
             if kwargs is not None and 'class_directory' in kwargs:
                 class_directory = kwargs['class_directory']
-            instance = AssetFactory.dynamic_create_instance_from_uri(class_object.objectUri, directory=class_directory)
+            instance = dynamic_create_instance_from_uri(class_object.objectUri, directory=class_directory)
             if instance is None:
                 continue
             instance._assetId.fill_with_dummy_data()
